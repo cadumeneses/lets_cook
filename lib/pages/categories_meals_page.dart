9f7c35c434506b3c853c5931320619229569ecb8
Carlos Eduardo <carloseduardoms646@gmail.com>
2022-08-31 12:19:41 -0300

    feat: add meal_item

--- lib/pages/categories_meals_page.dart
+++ lib/pages/categories_meals_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lets_cook/components/meal_item.dart';
+import 'package:lets_cook/data/dummy_data.dart';
 import '../models/category.dart';
 
 class CategoriesMealsPage extends StatelessWidget {
@@ -7,6 +9,9 @@ class CategoriesMealsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)?.settings.arguments as Category;
+    final categoriesMeals = dummyMeals.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
 
     return Scaffold(
         appBar: AppBar(
@@ -15,10 +20,11 @@ class CategoriesMealsPage extends StatelessWidget {
             style: Theme.of(context).textTheme.headline6,
           ),
         ),
-        body: const Center(
-          child: Text(
-            'Revanues of categories',
-          ),
+        body: ListView.builder(
+          itemCount: categoriesMeals.length,
+          itemBuilder: ((ctx, index) {
+            return MealItem(categoriesMeals[index]);
+          }),
         ));
   }
 }
